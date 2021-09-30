7ba651b1b48ed597f51ece70647f12d0c2555852
Jackson Dagger <jacksonddagger@gmail.com>
2021-09-29 17:51:41 -0700

    Fix playfair bugs

--- playfair.h
+++ playfair.h
@@ -1,6 +1,12 @@
 #ifndef PLAYFAIR_H
 #define PLAYFAIR_H
 
+struct params{
+    const char* ciphertext;
+    int len;
+    char* key;
+    double* retmae;
+};
 struct g2_freq {
     char gram[3];
     double freq;
@@ -18,16 +24,17 @@ struct g5_freq {
     double freq;
 };
 
-double test_rand_keys(char *ciphertext, int len, char *key);
-double test_rand_key(char *ciphertext, int len, char *key);
+void *threaded_rand(void *_args);
+double test_rand_keys(const char *ciphertext, int len, char *key);
+double test_rand_key(const char *ciphertext, int len, char *key);
 void random_swap(char *key);
 void random_key(char *key);
-double mae(char key[], char* ciphertext, int len);
+double mae(char key[], const char *ciphertext, int len);
 double digram_mae(char* plaintext, int len);
 double inline_digram_mae(char* plaintext, int len);
 double trigram_mae(char* plaintext, int len);
-char* playfair_decode(char key[], char* ciphertext, int len);
-int digram_decrypt(char * in, char * out, int key_mappings[25], char key[]);
+char* playfair_decode(char key[], const char *ciphertext, int len);
+int digram_decrypt(const char * in, char * out, const int key_mappings[25], const char key[]);
 int char_pos(char in);
 int pos_char(int pos);
 
